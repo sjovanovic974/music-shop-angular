2691dcbf99e91faf6885fc22c48456001ca2c9aa
sjovanovic974 <sjovanovic974@gmail.com>
2022-02-20 16:17:11 +0100

    Login/Logout - Okta Integration added. Closes #24

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oa3xhmxbwqRjo65P5d7',
+    issuer: 'https://dev-77483866.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
